5e8125c5784ee24c64fffac0152f160c62010a45
Danna Patricia Riveroll <dannap7337@gmail.com>
2026-04-24 01:50:45 -0600

    Refactor dashboard tickets code for clarity

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-st.set_page_config(page_title="Dashboard Tickets - Sincronizado", layout="wide")
+st.set_page_config(page_title="Dashboard Tickets", layout="wide")
 
 @st.cache_data
 def cargar_datos_pro():
